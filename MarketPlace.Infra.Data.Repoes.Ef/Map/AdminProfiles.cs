15e41dd5026469f438a747b60130c1808b3fac5a
mmd t <mhmdttlbzd@gmail.com>
2023-10-28 13:02:58 +0300

    .

--- MarketPlace.Infra.Data.Repoes.Ef/Map/AdminProfiles.cs
+++ MarketPlace.Infra.Data.Repoes.Ef/Map/AdminProfiles.cs
@@ -1,11 +1,6 @@
 ﻿using AutoMapper;
 using MarketPlace.Domain.Core.Application.Dtos;
 using MarketPlace.Domain.Core.Application.Entities._Admin;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MarketPlace.Infra.Data.Repoes.Ef.Map
 {
